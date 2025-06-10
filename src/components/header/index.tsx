6d936bc09ab1325120e59014b2284effc257cb0f
renelps <renanlps616@gmail.com>
2025-06-10 16:15:16 -0300

    feat(header): integrate NextAuth session handling into Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,23 @@
-import Link from "next/link";
-import { FiUser, FiLogOut } from "react-icons/fi";
+"use client"
 
+import Link from "next/link";
+import { FiUser, FiLogOut, FiLoader, FiLock } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header(){
+
+  const { status, data } = useSession();
+
+  async function handleSignIn() {
+    await signIn();
+  }
+
+  async function handleSignOut() {
+    await signOut();
+  }
+
+
+
   return (
     <header className="w-full flex items-center justify-center px-2 h-20 py-4 shadow-sm">
       <div className="w-full flex items-center justify-between max-w-7xl">
@@ -16,17 +31,32 @@ export function Header(){
           </h1>
         </Link>
 
-
-        <div className="flex items-center gap-1">
-          <Link href="/dashboard">
-            <FiUser size={26} color="#4b5563" />
-          </Link>
-          <button>
-            <Link href="/login">
-              <FiLogOut size={26} color="#4b5563" />
-            </Link>
+        {status === "loading" && (
+          <button className="cursor-pointer animate-spin" >
+            <FiLoader size={26} color="#4b5563"/>
           </button>
-        </div>
+        )}
+
+        {status === "unauthenticated" && (
+          <button className="cursor-pointer" onClick={handleSignIn}>
+            <FiLock size={26} color="#4b5563"/>
+          </button>
+        )}
+
+        {status === "authenticated" && (
+          <div className="flex items-center gap-1">
+            <Link href="/dashboard">
+              <FiUser size={26} color="#4b5563" />
+            </Link>
+            <button onClick={handleSignOut}>
+              <Link href="/login">
+                <FiLogOut size={26} color="#4b5563" />
+              </Link>
+            </button>
+          </div>
+        )}
+
+
       </div>
     </header>
   )
