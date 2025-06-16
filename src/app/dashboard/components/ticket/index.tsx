217fe10b97cf1f401fdd6bb91a13840c45e57ce8
renelps <renanlps616@gmail.com>
2025-06-16 07:30:53 -0300

    feat(dashboard): add dashboard layout and ticket display component

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,19 +1,55 @@
-import { FiFile, FiTrash2 } from "react-icons/fi";
+"use client"
 
-export function TicketItem(){
+import { api } from "@/lib/api";
+import { formatDate } from "@/utils/date";
+import { CustomerProps } from "@/utils/types/customer.type";
+import { TicketProps } from "@/utils/types/tickets.type";
+import { FiCheckSquare, FiFile } from "react-icons/fi";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import { ModalContext } from "@/providers/modal";
+interface TicketItemProps {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
+}
+
+export function TicketItem({ ticket, customer }: TicketItemProps){
+  const { handleModalVisible, setDetailTicket } = useContext(ModalContext);
+  const router = useRouter()
+  async function handleChangeStatus(){
+    try {
+      await api.patch("/api/ticket", {
+        id: ticket.id
+      })
+
+      router.refresh()
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  function handleOpenModal(){
+    handleModalVisible()
+    setDetailTicket({ ticket: ticket, customer: customer })
+  }
+
+
   return (
     <>
-      <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-slate-200 duration-200">
-        <td className="text-left pl-1">Renan Gabriel</td>
-        <td className="text-left hidden sm:table-cell">10/06/2025</td>
+      <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-slate-200 duration-200 w-full mb-10 rounded">
+        <td className="text-left pl-3">{customer?.name}</td>
+        <td className="text-left hidden sm:table-cell">
+          {formatDate(ticket.created_at)}
+        </td>
         <td className="text-left">
           <span className="bg-green-500 py-1 px-2">Aberto</span>
         </td>
         <td className="text-left">
-          <button className="mr-2"> 
-            <FiTrash2 size={24} color="#EF4444"/>
+          <button className="mr-2 cursor-pointer" onClick={handleChangeStatus}> 
+            <FiCheckSquare size={24} color="#016630"/>
           </button>
-          <button>
+          <button className="cursor-pointer" onClick={handleOpenModal}>
             <FiFile size={24} color="#3b82f6"/>
           </button>
         </td>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,9 @@ import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import Link from "next/link";
 import { TicketItem } from "./components/ticket";
-
-
+import prismaClient from "@/lib/prisma"
+import { ModalTicket } from "@/components/modal";
+import { ButtonRefresh } from "./components/buttonReflesh";
 
 export default async function DashBoard(){
 
@@ -13,17 +14,38 @@ export default async function DashBoard(){
 
   if(!session || !session?.user) redirect("/")
 
+    const tickets = await prismaClient.ticket.findMany({
+      where: {
+        status: "ABERTO",
+        customer: {
+          userId: session.user.id
+        }
+      }, 
+      include: {
+        customer: true,
+      },
+      orderBy: {
+        created_at: "desc"
+      }
+    })
+
   return (
     <Container>
-      <main className="my-2">
+      <main className="my-2 relative">
+       <ModalTicket />
+
+
         <div className="flex items-center justify-between">
           <h2 className="font-bold text-2xl">Chamados</h2>
-          <Link href="/dashboard/customer/new" className="font-medium bg-blue-500 text-white py-2 px-5 rounded-sm">
-            cadastrar
-          </Link>
+          <div className="flex items-center gap-4">
+            <Link href="/dashboard/customer/new" className="font-medium bg-blue-500 text-white py-2 px-5 rounded-sm">
+              cadastrar
+            </Link>
+            <ButtonRefresh />
+          </div>
         </div>
 
-        <table className="min-w-full my-2">
+        <table className="min-w-full my-2 w-full border-separate border-spacing-y-4">
           <thead>
             <tr>
               <th className="font-medium text-left pl-1">CLIENTE</th>
@@ -34,10 +56,24 @@ export default async function DashBoard(){
           </thead>
 
           <tbody>
-            <TicketItem />
+            {tickets.map((item) => (
+              <TicketItem 
+                key={item.id} 
+                ticket={item}
+                customer={item.customer}
+              />
+            ))}
           </tbody>
 
         </table>
+
+        {tickets.length === 0 && 
+          <p className="text-center mt-5">Você ainda não tem nenhum chamado, 
+            <Link href="/dashboard/customer" className="text-blue-600 border-b-1 pl-1">
+              Cadastrar chamado
+            </Link>
+          </p>
+        }
       </main>
     </Container>
   )
